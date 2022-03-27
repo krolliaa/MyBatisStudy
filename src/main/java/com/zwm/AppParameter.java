c6bf02b6d4b911e23eade5ec23cda43930e8950d
krolliaa <krolliaa@163.com>
2022-03-27 23:36:06 +0800

    @Param 传递参数

---
+++ src/main/java/com/zwm/AppParameter.java
@@ -0,0 +1,21 @@
+package com.zwm;
+
+import com.zwm.mapper.UserMapper;
+import com.zwm.pojo.User;
+import com.zwm.util.MyBatisUtil;
+
+public class AppParameter {
+    /*
+        MyBatis 可以传递单个参数可以直接传，格式为：#{属性名}，属性可以是基本数据类型也可以是 String 类型
+        MyBatis 也可以传递多个参数：
+        1. 以对象的形式传递多个参数，可以直接使用 #{属性名} 的形式传递，前提是该对象的类属性名和数据库名保持一直
+           那如果不保持一致该如何修改呢？此时就要修改 UserMapper.xml 中的 resultType 改为 resultMap 这个后面会学习到
+        2. 以 @Param 的形式传递 ---> User selectUserByUsername(@Param(‘myName) String username);
+
+    */
+    public static void main(String[] args) {
+        UserMapper userMapper = MyBatisUtil.getSqlSession().getMapper(UserMapper.class);
+        User user = userMapper.selectUserByUserName("AA");
+        System.out.println(user.toString());
+    }
+}
--- src/main/java/com/zwm/mapper/UserMapper.java
+++ src/main/java/com/zwm/mapper/UserMapper.java
@@ -1,10 +1,12 @@
 package com.zwm.mapper;
 
 import com.zwm.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface UserMapper {
     public abstract User selectUserById(int id);
     public abstract List<User> selectAllUsers();
+    public abstract User selectUserByUserName(@Param("myName") String username);
 }

169ea5eba59291562da33631c4c9a42fc016907a
krolliaa <krolliaa@163.com>
2022-03-27 23:48:57 +0800

    #{arg0} 位置形式传递参数

--- src/main/java/com/zwm/AppParameter.java
+++ src/main/java/com/zwm/AppParameter.java
@@ -11,11 +11,14 @@ public class AppParameter {
         1. 以对象的形式传递多个参数，可以直接使用 #{属性名} 的形式传递，前提是该对象的类属性名和数据库名保持一直
            那如果不保持一致该如何修改呢？此时就要修改 UserMapper.xml 中的 resultType 改为 resultMap 这个后面会学习到
         2. 以 @Param 的形式传递 ---> User selectUserByUsername(@Param(‘myName) String username);
-
+        3. 以位置形式传递参数，格式如：#{arg0}, #{arg1}
     */
     public static void main(String[] args) {
         UserMapper userMapper = MyBatisUtil.getSqlSession().getMapper(UserMapper.class);
         User user = userMapper.selectUserByUserName("AA");
+        System.out.println(user.toString());
+        System.out.println(userMapper.insertUser(new User(11, "AK", "123456")));
+        user = userMapper.selectUserByIdAndPassword(1, "123456");
         System.out.println(user.toString());
     }
 }
--- src/main/java/com/zwm/mapper/UserMapper.java
+++ src/main/java/com/zwm/mapper/UserMapper.java
@@ -9,4 +9,6 @@ public interface UserMapper {
     public abstract User selectUserById(int id);
     public abstract List<User> selectAllUsers();
     public abstract User selectUserByUserName(@Param("myName") String username);
+    public abstract int insertUser(User user);
+    public abstract User selectUserByIdAndPassword(int id, String password);
 }
